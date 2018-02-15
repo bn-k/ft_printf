fb5b3e6249644ed8845d6e64d5a5fc9ccac51f98
Abdelkrim BENHAMMOU <newcratie@gmail.com>
2018-02-15 19:54:28 +0100

    format adress ok, %d ok

--- srcs/main.c
+++ srcs/main.c
@@ -1,23 +1,26 @@
-#include "ft_printf.h"
+#include "../libft/includes/ft_printf.h"
 #include <stdio.h>
+#include <locale.h>
 
-void	test(wchar_t *wc)
-{
-	ft_putstr_utf8(wc);
-}
+void	test(void);
+void	test_1(void);
+void	test_2(void);
+void	test_3(char *s, char *s2);
+
 
 int	main(int ac, char **av)
 {
+		setlocale(LC_ALL, "en_US.UTF-8");
 	if (ac == 1)
-		test_1();
-	if (av[1])
+		test();
+	else if (av[1])
 	{
 		if (av[1][0] == '1')
 			test_1();
 		if (av[1][0] == '2')
 			test_2();
-		else 
-			test_3(av[1], "string");
+		if (ac == 3)
+			test_3(av[1], av[2]);
 	}
 	return (0);
 }
